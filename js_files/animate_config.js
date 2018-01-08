d931f17e4c976aefc3327464c78e11854b8cae20
ANKITKSANIL <ankitksanil05@gmail.com>
2018-01-08 11:17:17 +0530

    first commit of all

---
+++ js_files/animate_config.js
@@ -0,0 +1,16 @@
+
+// $(document).ready(function(){
+//     var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+//      $("#header_col_1").addClass("animated bounceInLeft");
+//      $("#header_col_2").addClass("animated bounceInRight");
+//      $("#service_col_1").addClass("animated fadeInLeft");
+//      $("#service_col_2").addClass("animated fadeInLeft");
+//      $("#service_col_3").addClass("animated fadeInLeft");
+//      $("#service_col_4").addClass("animated fadeInLeft");
+//      $("#pricing_col_1").addClass("animated fadeInLeft");
+//      $("#pricing_col_2").addClass("animated fadeInLeft");
+//      $("#pricing_col_3").addClass("animated fadeInLeft");
+//      $("#pricing_col_4").addClass("animated fadeInLeft");
+
+// })
+
